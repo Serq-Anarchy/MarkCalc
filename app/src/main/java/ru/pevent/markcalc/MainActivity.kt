e48cd331b0454aea328bc1447e691b6f8a8e005d
Seq-Anarq <sergo54ru@gmail.com>
2022-06-22 17:03:30 +0700

    Initial commit

---
+++ app/src/main/java/ru/pevent/markcalc/MainActivity.kt
@@ -0,0 +1,37 @@
+package ru.pevent.markcalc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.pevent.markcalc.ui.theme.MarkCalcTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MarkCalcTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
+@ExperimentalFoundationApi
+@Composable
+fun App() {
+
+}
